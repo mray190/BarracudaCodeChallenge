e56f8ab862d3bd7304f8ef1ac6adac3288e1e74c
Shane Schulte <schulte.shane@gmail.com>
2014-10-17 21:15:45 -0400

    AlgoDeath6969's Supreme Takeover Has Begun

---
+++ src/GodzillasToothbrush/Algorithms/AlgoDeath6969.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GodzillasToothbrush.Algorithms;
+
+import GodzillasToothbrush.Board;
+import GodzillasToothbrush.Point;
+
+/**
+ *
+ * @author shane
+ */
+public class AlgoDeath6969 extends Algorithm {
+    @Override
+    public Point makeMove(Board current, Board previous) {
+        return null;
+    }
+    
+}
